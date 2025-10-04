8828c5b2c8251d802f52189a0f2cf5776c28f2fa
Gaurav Singh <71246220+vanhalen42@users.noreply.github.com>
2025-10-04 18:47:07 -0400

    Rename package to cgdf

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
         requires_list.append(str(line))
 
 
-setup(name='se3dif',
+setup(name='cgdf',
       version=__version__,
-      description='SE(3)-DiffusionFields. A library to learn and sample from diffusion models on SE(3). We show how to train and use them for learning 6D grasp distributions.',
-      author='Julen Urain',
-      author_email='julen@robot-learning.de',
-      packages=['se3dif'],
+      description='Constrained Grasp Diffusion Fields',
+      author='Gaurav Singh',
+      author_email='gaurav@brown.edu',
+      packages=['cgdf'],
       install_requires=requires_list,
       )
